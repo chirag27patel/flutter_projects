12d30478d935838dd953c112d0cc6b61e74a90c8
Chilu <c27chilu@gmail.com>
2022-06-20 18:13:43 +0530

    push from chilu pc

--- lib/admin/admin_home.dart
+++ lib/admin/admin_home.dart
@@ -20,74 +20,185 @@ class _AdminHomeState extends State<AdminHome> {
         title: Text("Admin Home"),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(4.0),
-            child: Row(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                AdminHomeTab(
-                  imagePath: "assets/images/car.png",
-                  label: "L&t today's Trips",
-                  labelDetails: "22",
+                Padding(
+                  padding: const EdgeInsets.only(top: 6.0, left: 8),
+                  child: Text(
+                    "Today's Casual Trips",
+                    style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),
+                  ),
                 ),
-                AdminHomeTab(
-                  imagePath: "assets/images/car.png",
-                  label: "AMNS Trips",
-                  labelDetails: "22",
-                ),
-
               ],
             ),
-          ),
-
-        ],
+            Container(
+              padding: EdgeInsets.only(left: 4.0,),
+              child: Row(
+                children: [
+                  AdminHomeTab(
+                    imagePath: "assets/images/landt.png",
+                    label: "L&T",
+                    labelDetails: "22",
+                    tabColor: Colors.yellow.shade600,
+                    tabTextColor: Colors.white,
+                  ),
+                  AdminHomeTab(
+                    imagePath: "assets/images/amns.png",
+                    label: "AM/NS",
+                    labelDetails: "22",
+                    tabColor: Colors.redAccent.shade200,
+                    tabTextColor: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 12.0, left: 8),
+                  child: Text(
+                    "Monthly Car Details",
+                    style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 4.0,right: 4.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      AdminHomeTab(
+                        imagePath: "assets/images/car.png",
+                        label: "Car List",
+                        labelDetails: "22",
+                        tabColor: Colors.lightBlueAccent,
+                        tabTextColor: Colors.white,
+                      ),
+                      AdminHomeTab(
+                        imagePath: "assets/images/documentlist.png",
+                        label: "Car Documetns",
+                        labelDetails: "22",
+                        tabColor: Colors.orangeAccent,
+                        tabTextColor: Colors.red.shade800,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      AdminHomeTab(
+                        imagePath: "assets/images/casual_duty.png",
+                        label: "Casual Duty",
+                        labelDetails: "22",
+                        tabColor: Colors.teal,
+                        tabTextColor: Colors.white,
+                      ),
+                      AdminHomeTab(
+                        imagePath: "assets/images/log.png",
+                        label: "Logbook",
+                        labelDetails: "22",
+                        tabColor: Colors.redAccent,
+                        tabTextColor: Colors.white,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      AdminHomeTab(
+                        imagePath: "assets/images/fuel.png",
+                        label: "Diesel",
+                        labelDetails: "27",
+                        tabColor: Colors.blueGrey,
+                        tabTextColor: Colors.white,
+                      ),
+                      AdminHomeTab(
+                        imagePath: "assets/images/clock.png",
+                        label: "Logbook History",
+                        labelDetails: "00",
+                        tabColor: Colors.orange.shade800,
+                        tabTextColor: Colors.white,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
-
 class AdminHomeTab extends StatelessWidget {
-
   String label;
   String labelDetails;
-  String? imagePath;
+  String imagePath;
+  Color tabColor;
+  Color tabTextColor;
 
-  AdminHomeTab({required this.label,required this.labelDetails, required this.imagePath});
+  AdminHomeTab(
+      {required this.label,
+      required this.labelDetails,
+      required this.imagePath,
+      required this.tabColor,
+      required this.tabTextColor,
+      });
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
-        margin: EdgeInsets.only(left: 4.0,right: 4.0,top: 4.0,bottom: 4.0),
+        margin: EdgeInsets.only(left: 4.0, right: 4.0, top: 4.0, bottom: 4.0),
         child: Material(
           borderRadius: BorderRadius.circular(5.0),
-          color: Colors.orangeAccent,
+          color: tabColor,
           child: InkWell(
-            onTap: (){},
+            onTap: () {},
             borderRadius: BorderRadius.circular(5.0),
+            splashColor: Colors.black87,
             child: Row(
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(left: 12.0,right: 4.0),
-                  child: Image.asset(imagePath!,height: 40,width: 40,),
+                  padding: const EdgeInsets.only(left: 12.0, right: 4.0),
+                  child: Image.asset(
+                    imagePath,
+                    height: 100,
+                    width: 50,
+
+                  ),
                 ),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Padding(
-                      padding: const EdgeInsets.only(left: 5,top: 16,bottom: 0,right: 0),
-                      child: Text(label,style: TextStyle(fontSize: 14,color: Colors.white),),
+                      padding: const EdgeInsets.only(
+                          left: 5, top: 16, bottom: 0, right: 0),
+                      child: Text(
+                        label,
+                        style: TextStyle(fontSize: 14, color: tabTextColor),
+                      ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(left: 5,top: 8,bottom: 16,right: 0),
-                      child: Text(labelDetails,style: TextStyle(fontSize: 28,fontWeight: FontWeight.bold,color: Colors.white),),
+                      padding: const EdgeInsets.only(
+                          left: 5, top: 0, bottom: 16, right: 0),
+                      child: Text(
+                        labelDetails,
+                        style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.w800,
+                            color: tabTextColor),
+                      ),
                     ),
                   ],
                 ),
               ],
-
             ),
           ),
         ),
--- lib/const/const.dart
+++ lib/const/const.dart
@@ -7,4 +7,4 @@ Color bgColor = Colors.grey.shade200;
 const Color selectedIconColor = Colors.white;
 const Color unselectedIconColor = Color(0xff364781);
 Color textColor = Colors.grey.shade700;
-const Color adminAppbarColor = Colors.deepOrangeAccent;
+const Color adminAppbarColor = Colors.orangeAccent;
