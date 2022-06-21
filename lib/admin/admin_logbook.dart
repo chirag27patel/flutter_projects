1b2d310b647ccfd037062e5d2d98ebea9dd80469
Chilu <c27chilu@gmail.com>
2022-06-21 17:17:38 +0530

    push from chilu pc

--- lib/admin/admin_logbook.dart
+++ lib/admin/admin_logbook.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:sejaltravels_2022/const/const.dart';
@@ -10,6 +12,20 @@ class AdminLogbook extends StatefulWidget {
 }
 
 class _AdminLogbookState extends State<AdminLogbook> {
+  List<String> selectCar = ["Innova", "Swift", "Swift Dzire", "Xylo", "Tavera"];
+
+  // Initial Selected Value
+  String dropdownvalue = 'Item 1';
+
+  // List of items in our dropdown menu
+  List<String> items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,37 +35,47 @@ class _AdminLogbookState extends State<AdminLogbook> {
         title: Text("Admin Logbook"),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(8.0),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black87,width: 1),
-              borderRadius: BorderRadius.circular(5.0)
-            ),
-            child: Row(
-              children: [
-                Expanded(
-                  child: DropdownButtonHideUnderline(
-                    child: DropdownButton<String>(
-                      iconSize: 0.0,
-                      style: TextStyle(color: Colors.black),
-                      underline: SizedBox(),
-                      items: <String>['A', 'B', 'C', 'D'].map((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      onChanged: (_) {},
-                    ),
-                  ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.only(
+                left: 16,
+                right: 16,
+              ),
+              padding: EdgeInsets.only(right: 10, left: 10),
+              decoration: BoxDecoration(
+                border: Border.all(width: 1, color: Colors.black),
+                borderRadius: BorderRadius.circular(5.0)
+              ),
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton(
+                  // Initial Value
+                  value: dropdownvalue,
+                  isExpanded: true,
+                  // Down Arrow Icon
+                  icon: const Icon(Icons.keyboard_arrow_down),
+
+                  // Array list of items
+                  items: items.map((String items) {
+                    return DropdownMenuItem(
+                      value: items,
+                      child: Text(items),
+                    );
+                  }).toList(),
+                  // After selecting the desired option,it will
+                  // change button value to selected value
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownvalue = newValue!;
+                    });
+                  },
                 ),
-                Icon(Icons.arrow_drop_down_outlined),
-              ],
+              ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
