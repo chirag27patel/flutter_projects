caf4685a2562dc8dc2c7b6e4dd84c1c95a4a1f30
Chilu <c27chilu@gmail.com>
2022-07-19 11:10:45 +0530

    push from chilu pc

--- lib/main.dart
+++ lib/main.dart
@@ -17,23 +17,8 @@ class MyApp extends StatelessWidget {
         fontFamily: 'dmsans',
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const SignInPage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return  SignInPage();
   }
 }
 
